0f7fede5573560c62173260f0d8ab069504a8af9
to_be_peace_da <nikita.nik.u@mail.ru>
2022-01-19 21:34:05 +1000

    change's

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -43,7 +43,7 @@ function reveal(){
 
         let windowWeight = window.innerHeight;
         let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 400;
+        let revealPoint = 300;
 
         if(revealTop < windowWeight - revealPoint){
             reveals[i].classList.add('active');
